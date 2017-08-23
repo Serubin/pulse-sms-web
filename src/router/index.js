50d1f0fd46066aa370aa7753d9102b2514a62caa
Serubin <Serubin@serubin.net>
2017-08-23 14:29:18 -0400

    Update router to conversation list

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Conversations from '@/components/Conversations.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'Hello',
-            component: Hello
+            name: 'conversations-list',
+            component: Conversations
         }
     ]
 })
