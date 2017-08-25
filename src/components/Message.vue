cc870cd2ace363d566fe93c693a81d46fc301374
Serubin <Serubin@Serubin.net>
2017-08-25 10:00:50 -0400

    Create message comp

---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="message-wrapper" :title="stringTime">
+        <div :class="{'message-round': round, 'message': !round}" :class="msgType" :style="{background: color, borderColor: color transparent}" :id="id">
+            {{ message }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'message',
+    props: [],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+    @import "../assets/scss/_vars.scss";
+
+</style>
