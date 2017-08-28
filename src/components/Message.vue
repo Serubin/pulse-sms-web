642c3ed0593a6e8bf3651c642fd03e704678cf87
Serubin <Serubin@serubin.net>
2017-08-28 13:41:39 -0400

    Add styling to message comp

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="message-wrapper" :title="stringTime">
-        <div :class="style_class"  :style="{background: color, borderColor: [color, 'transparent'] }" :id="id">
+        <div :class="style_class"  :style="styleGenerator" :id="id">
             {{ content }}
         </div>
     </div>
@@ -30,7 +30,8 @@ export default {
         switch ( this.type ) {
             case 0:
             case 6: { 
-                this.color = this.thread_color;
+                this.color = this.threadColor;
+                this.style_class.push('received');
                 break;
             }
             case 3: {
@@ -56,7 +57,7 @@ export default {
             content: this.messageData.data,
             type: this.messageData.message_type,
             timestamp: this.messageData.timestamp,
-            color: '',
+            color: 'rgba(255,255,255,1)',
             style_class: [ ]
         }
     },
@@ -68,9 +69,11 @@ export default {
         stringTime () {
             return new Date(this.timestamp).toLocaleString()
         },
-        msgType () {
-            return '';
-        },
+        styleGenerator () {
+            return "background: " + this.color + ";"
+                + "border-color: " + this.color
+                + " transparent;"
+        }
     }
 }
 </script>
@@ -78,5 +81,83 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
     @import "../assets/scss/_vars.scss";
+
+    .message-wrapper {
+        user-select: text;
+        clear: both;
+        display: block;
+    }
+
+    .message, .message-round {
+        position: relative;
+        padding: 16px;
+        margin: 4px 8px 4px 8px;
+        max-width: 310px;
+        border-radius: 2px;
+        overflow-wrap: break-word;
+        word-wrap: break-word;
+        min-width: 18px;
+
+        &:after {
+            content: "";
+            position: absolute;
+            bottom: -20px;
+            left: 50px;
+            border-style: solid;
+            display: block;
+            top: 0px;
+            bottom: auto;
+        }
+    }
+
+    .received {
+        float: left;
+        color: white;
+        margin-left: 28px;
+        background: white;
+        border-color: red transparent;
+
+        &:after {
+            left: -18px;
+            border-width: 15px 0 0 20px;
+            border-color: inherit;
+        }
+    }
+
+    .sent {
+        float: right;
+        color: black;
+        margin-right: 28px;
+        background: white;
+
+        &:after {
+            right: -18px;
+            border-width: 15px 20px 0 0;
+            border-color: white transparent;
+        }
+    }
+    
+    .error {
+        float: right;
+        color: black;
+        margin-right: 28px;
+        background: #F44336;
+        color: white;
+
+        &:after {
+            right: -18px;
+            border-width: 15px 20px 0 0;
+            border-color: #F44336 transparent;
+        }
+    }
+
+    .info {
+        text-align: center;
+        margin: auto;
+
+        &:after {
+            border-color: transparent transparent;
+        }
+    }
 
 </style>
