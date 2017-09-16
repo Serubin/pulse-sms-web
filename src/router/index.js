83bea3a581e7fadf91f5ff43945543211c03cba3
Serubin <Serubin@Serubin.net>
2017-09-16 01:38:12 -0400

    Add route gaurding

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import store from '@/store/'
+
 import Login from '@/components/Login.vue'
 import Thread from '@/components/Thread.vue'
 import Conversations from '@/components/Conversations.vue'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+let router = new VueRouter({
     mode: "history",
     routes: [
         { 
@@ -33,5 +35,18 @@ export default new VueRouter({
             name: 'login',
             component: Login,
         },
-    ]
+    ],
 })
+    
+router.beforeEach((to, from, next) => {
+    
+    if(to.name == 'login')
+        return next();
+
+    if( store.state.account_id == '' )
+        return next({name: 'login'});
+
+    next();
+})
+
+export default router
