08a91aaa95e1fdbc06622134c34592d09f4ccc5b
Luke Klinker <lklinker1@gmail.com>
2019-02-26 13:47:43 -0600

    show text in the 'Account' section on how to upgrade to the lifetime subscription, if the user is not already a lifetime subscriber

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -135,6 +135,9 @@ export const i18n = new VueI18n({
                 yes: 'Yes',
                 no: 'No'
             },
+            account: {
+                upgrade_subscription: 'Want to change or upgrade your subscription? Head to the "My Account" section, in the Android app!'
+            },
             experiments: {
                 disclaimer: 'Disclaimer',
                 explanatory_intro: 'These preferences come with no support and no guarentee that they will continue to exist. As an open source app, they will be useful to test new features and provide developers with the means of implementing their own tweaks into the app. Use with caution and do not rely on them, unless you are maintaining them. They are meant to be experiments.',
