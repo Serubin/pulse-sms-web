331cc3d6ac34b4610962bc9fb8cb619c606cd844
Luke Klinker <lklinker1@gmail.com>
2019-04-25 06:26:27 -0500

    remove Google+ link from the help and feedback page

--- src/components/HelpFeedback.vue
+++ src/components/HelpFeedback.vue
@@ -2,15 +2,11 @@
      <div class="page-content" id="account-list" v-mdl>
         <h4>Get Help</h4>
         <div class="item">
-            <div class="mdl-color-text--grey-600">I have created a very extensive FAQ section for the app.</br>As a one-man crew, I get a significant amount of emails, and they cut heavily into my development time. Before sending an email, do a quick search to free up time for both of us. You will probably find your answer very easily.</br></br>If you don't find what you are looking for, feel free to reach out and I will be more than happy to get back to you as quickly as I am able.</div>
+            <div class="mdl-color-text--grey-600">I have created a very extensive FAQ section for the app.</br>As a one-man crew, I get a significant amount of emails, and they cut heavily into my development time. Before sending an email, do a quick search to free up time for both of us. You will probably find your answer, very easily.</br></br>If you don't find what you are looking for, feel free to reach out and I will be more than happy to get back to you as quickly as I am able.</div>
         </div>
         <div class="click-item" @click="routeTo('https://messenger.klinkerapps.com/help')">
             <div class="mdl-color-text--grey-900">Help Page</div>
             <div class="mdl-color-text--grey-600">Search here, first!</div>
-        </div>
-        <div class="click-item" @click="routeTo('https://plus.google.com/u/0/communities/110320018522684513593')">
-            <div class="mdl-color-text--grey-900">Google+</div>
-            <div class="mdl-color-text--grey-600">Messenger Beta Community</div>
         </div>
         <div class="click-item" @click="routeTo('https://twitter.com/klinkerapps')">
             <div class="mdl-color-text--grey-900">Twitter</div>
@@ -30,7 +26,7 @@
 
         <h4>Contributions</h4>
         <div class="item">
-             <div class="mdl-color-text--grey-900">© 2018 Luke and Jake Klinker</div>
+             <div class="mdl-color-text--grey-900">© 2019 Luke and Jake Klinker</div>
         </div>
         <div class="click-item" @click="routeTo('https://github.com/klinker24/messenger-web/')">
             <div class="mdl-color-text--grey-900">Pulse's web app is open-source</div>
