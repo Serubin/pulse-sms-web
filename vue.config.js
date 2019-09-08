5a02927d33926833b491830ac677fec6eed17a55
Serubin <Serubin@Serubin.net>
2019-09-08 12:12:29 -0400

    Add service-worker configurations

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+
 module.exports = {
   runtimeCompiler: true,
-  lintOnSave: false
+    lintOnSave: false,
+    configureWebpack: {
+        plugins: [
+            new SWPrecacheWebpackPlugin({
+                cacheId: 'pulse-sms',
+                filename: 'service-worker.js',
+                staticFileGlobs: ['dist/**/*.{js,css}', '/'],
+                minify: true,
+                stripPrefix: 'dist/',
+                dontCacheBustUrlsMatching: /\.\w{6}\./
+            })
+        ]
+    },
+    chainWebpack: config => {
+        // GraphQL Loader
+        config.module
+            .rule('worker')
+            .test(/\.worker\.js$/)
+            .use('worker-loader')
+            .loader('worker-loader')
+            .end()
+  }
 }
