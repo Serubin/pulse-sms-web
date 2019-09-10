a756a6b1817f81cf21aa6a311409ac4a0a3229a3
Serubin <Serubin@Serubin.net>
2019-09-10 10:20:26 -0400

    Increase linting to recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,15 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     'eslint:recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': ["error", 4, { "SwitchCase": 1 }]
+      'indent': ["error", 4, { "SwitchCase": 1 }],
+      'vue/max-attributes-per-line': 0,
+      "vue/html-indent":  ["error", 4, {}]
   },
   parserOptions: {
     parser: 'babel-eslint'
