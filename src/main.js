9bfbd6994605f60220e5aae876baf1d8ec848261
Serubin <serubin@serubin.net>
2020-03-09 20:59:06 -0400

    Update bugsnag with better context reporting

--- src/main.js
+++ src/main.js
@@ -18,7 +18,22 @@ import { componentHandler } from '@/lib/material.js';
 Vue.config.productionTip = false;
 
 if (!Util.isDevMode()) {
-    const bugsnagClient = bugsnag('c9e7daf16e171be27f206895b77cee70', { appVersion: version });
+    // Modify report before sending to bugsnag
+    const beforeSend = (report) => {
+        const url = report.context;
+
+        // Change context for thread to better group errors
+        if (url.includes('/thread/')) {
+            const m = report.context.match(/(.*\/thread\/)(\d+)(.*)/);
+            report.context = `${m[1]}:id${m[3]}`; // Rewrite as `/thread/:id/tail`
+        }
+    };
+
+    const bugsnagClient = bugsnag({
+        beforeSend,
+        apiKey: 'c9e7daf16e171be27f206895b77cee70',
+        appVersion: version
+    });
     bugsnagClient.use(bugsnagVue, Vue);
 }
 
