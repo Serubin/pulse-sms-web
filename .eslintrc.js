c433e50249306b8349717dfc8a350c1b20ae80ce
Serubin <serubin@serubin.net>
2021-01-08 00:13:10 -0800

    Fix eslint configs for vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,48 +3,43 @@ module.exports = {
   env: {
     node: true
   },
-  settings: {
-    'import/resolver': {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      },
-      '@':  './',
-    }
-  },
   'extends': [
-    'plugin:vue/recommended',
     'eslint:recommended',
-    'eslint-config-airbnb-base'
+    'plugin:vue/essential',
+    '@vue/standard'
   ],
   'plugins': [
     'standard',
-    'import',
   ],
     rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': ["error", 4, { "SwitchCase": 1 }],
-    'vue/max-attributes-per-line': "off",
-    'vue/require-prop-types': "off",
-    "vue/html-indent":  ["error", 4, {}],
-    "vue/html-self-closing": ["error", {
-      "html": {
-       "void": "never",
-       "normal": "never",
-       "component": "always"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 4, { SwitchCase: 1 }],
+    'vue/max-attributes-per-line': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/html-indent':  ['error', 4, {}],
+    'vue/html-self-closing': ['error', {
+      html: {
+       'void': 'never',
+       normal: 'never',
+       component: 'always'
       },
-      "svg": "never",
-      "math": "never"
+      svg: 'never',
+      math: 'never'
     }],
-    semi: ["error", "always", { "omitLastInOneLineBlock": true }]
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }]
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    babelOptions: {
+      configFile: './babel.config.js',
+    },
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)'
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
