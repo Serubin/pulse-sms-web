686c6e3fce6f8fd150d1a1a485ef537e71578647
Serubin <serubin@serubin.net>
2021-01-24 01:30:16 -0800

    Add additional license notices

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const webpack = require('webpack');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 module.exports = {
     devServer: {
@@ -15,8 +16,9 @@ module.exports = {
                 minify: true,
                 stripPrefix: 'dist/',
                 dontCacheBustUrlsMatching: /\.\w{6}\./
-            })
+            }),
+            new webpack.BannerPlugin('PulseSMS Web - Copyright 2018 Solomon Rubin & Luke Klinker - Licensed under MIT and Apache 2.0 - this notice cannot be removed - admin@serubin.net')
         ]
-    },
-    //chainWebpack: config => {}
+    }
+    // chainWebpack: config => {}
 };
